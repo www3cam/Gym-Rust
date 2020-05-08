1cce69bd36973a1a5fc74678b0b952ae4ec0f3ba
www3cam <cameronfen@gmail.com>
2020-05-08 17:37:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='RustEnv1',
+setup(name='RustEnva',
       version='0.0.1',
       install_requires=['gym', 'numpy']  # And any other dependencies foo needs
 )
